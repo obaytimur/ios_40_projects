9d066ab7e7dfc1f131d8d66b7a732e3b09e6e20e
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-10-13 22:26:10 +0300

    feat: make two different counters

--- P2 Counter with gestures/P2 Counter with gestures/ContentView.swift
+++ P2 Counter with gestures/P2 Counter with gestures/ContentView.swift
@@ -8,39 +8,79 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter = 0
+    @State private var counter1 = 0
+    @State private var counter2 = 0
 
     var body: some View {
         NavigationStack{
-            VStack{
-                Spacer()
-                Text("\(counter)")
-                    .toolbar {
-                        Button(action: {
-                            counter = 0
-                        }) {
-                            Text("Reset")
-                                .font(.system(size: 25))
+            VStack {
+                HStack {
+                    VStack{
+                        Text("\(counter1)")
+                            .toolbar {
+                                ToolbarItem(placement: ToolbarItemPlacement.navigationBarLeading) {
+                                    Button(action: {
+                                        counter1 = 0
+                                    }) {
+                                        Text("Reset")
+                                            .font(.system(size: 25))
+                                    }
+                                }
+                            }
+                        Spacer()
+                            .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                        HStack{
+                            Button(action: {
+                                counter1 -= 1
+                            }) {
+                                Image(systemName: "minus")
+                                    .fixedSize()
+                            }
+                            .buttonRepeatBehavior(.enabled)
+                            Spacer()
+                                .frame(width: 15)
+                            Button(action: {
+                                counter1 += 1
+                            }) {
+                                Image(systemName: "plus")
+                                    .fixedSize()
+                            }
+                            .buttonRepeatBehavior(.enabled)
                         }
                     }
-                Spacer()
-                HStack{
-                    Button(action: {
-                        counter -= 1
-                    }) {
-                        Image(systemName: "minus")
-                            .fixedSize()
-                    }
-                    .buttonRepeatBehavior(.enabled)
                     Spacer()
-                        .frame(width: 30)
-                    Button(action: {
-                        counter += 1
-                    }) {
-                        Image(systemName: "plus")
-                            .fixedSize()
+                        .frame(width: 50)
+                    VStack{
+                        Text("\(counter2)")
+                            .toolbar {
+                                Button(action: {
+                                    counter2 = 0
+                                }) {
+                                    Text("Reset")
+                                        .font(.system(size: 25))
+                                }
+                            }
+                        Spacer()
+                            .frame(height: 100)
+                        HStack{
+                            Button(action: {
+                                counter2 -= 1
+                            }) {
+                                Image(systemName: "minus")
+                                    .fixedSize()
+                            }
+                            .buttonRepeatBehavior(.enabled)
+                            Spacer()
+                                .frame(width: 15)
+                            Button(action: {
+                                counter2 += 1
+                            }) {
+                                Image(systemName: "plus")
+                                    .fixedSize()
+                            }
+                            .buttonRepeatBehavior(.enabled)
+                        }
                     }
-                    .buttonRepeatBehavior(.enabled)
                 }
             }
             .navigationTitle("Counter")
