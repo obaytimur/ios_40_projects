d303968d8feb2d7f2d4bd252b079bf4eb3f5ed84
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-10-14 23:55:24 +0300

    feat: initialize app for iAd

---
+++ P3 IAD Execution/P3 IAD Execution/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  P3 iAd Execution
+//
+//  Created by Oğulcan Baytimur on 14/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ P3 IAD Execution/P3 IAD Execution/P3_IAD_ExecutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  P3_iAd_ExecutionApp.swift
+//  P3 iAd Execution
+//
+//  Created by Oğulcan Baytimur on 14/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct P3_iAd_ExecutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
