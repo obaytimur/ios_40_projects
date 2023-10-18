efd4bc8b031cb660d0e4a99f6c4e4c72dc2f268f
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-10-18 23:34:01 +0300

    feat: update info dict

--- P3 IAD Execution/P3 IAD Execution/ContentView.swift
+++ P3 IAD Execution/P3 IAD Execution/ContentView.swift
@@ -6,19 +6,48 @@
 //
 
 import SwiftUI
+import GoogleMobileAds
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+      VStack {
+        Text("Google Admob")
+        
+        GADBannerViewController()
+      }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+// MARK: - GADBannerViewController
+
+struct GADBannerViewController: UIViewControllerRepresentable {
+  
+  func makeUIViewController(context: Context) -> some UIViewController {
+    let view = GADBannerView(adSize: kGADAdSizeBanner)
+    let viewController = UIViewController()
+    let testID = "ca-app-pub-3940256099942544/2934735716"
+    let realID = "ca-app-pub-9450475706334809/7473547174"
+    
+    // Banner Ad
+    view.adUnitID = realID
+    view.rootViewController = viewController
+    
+    // View Controller
+    viewController.view.addSubview(view)
+    viewController.view.frame = CGRect(origin: .zero, size: kGADAdSizeBanner.size)
+    
+    // Load an ad
+    view.load(GADRequest())
+    
+    return viewController
+  }
+  
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+  
 }
