8b489d937c390a875f4ace34072293d31a33647e
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2023-10-24 18:42:59 +0300

    feat: move button to navigation bar and change its colour

--- P6 Date and Time/P6 Date and Time/ContentView.swift
+++ P6 Date and Time/P6 Date and Time/ContentView.swift
@@ -11,15 +11,25 @@ struct ContentView: View {
     @State private var todaysDate = Date.now.formatted(date: .long, time: .standard)
     
     var body: some View {
-        VStack {
-            Text("\(todaysDate)")
-            Button(action: {
-                todaysDate = Date.now.formatted(date: .long, time: .standard)
-            }, label: {
-                Text("Refresh")
-            })
+        NavigationView{
+            VStack {
+                Text("The Current Date and Time")
+                    .font(.headline)
+                Text("\(todaysDate)")
+                    .font(.title)
+                    .frame(height: 40)
+            }
+            .toolbar {
+                Button(action: {
+                    todaysDate = Date.now.formatted(date: .long, time: .standard)
+                }, label: {
+                    Text("Refresh")
+                        .foregroundColor(.white)
+                })
+            }
+            .toolbarBackground(.black, for: .navigationBar)
+            .toolbarBackground(.visible, for: .navigationBar)
         }
-        .padding()
     }
 }
 
