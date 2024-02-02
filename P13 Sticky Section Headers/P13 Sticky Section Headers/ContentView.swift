c45a2ba30a50cade3fc58ecc1e80d3c585c2d1a6
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-02 18:55:08 +0300

    feat: add sorted sections from dictionaries

--- P13 Sticky Section Headers/P13 Sticky Section Headers/ContentView.swift
+++ P13 Sticky Section Headers/P13 Sticky Section Headers/ContentView.swift
@@ -9,31 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sortedData = myData.sorted { $0["title"]! < $1["title"]!}
-    @State private var sectionHeaderKeys: [String] = Array()
-    @State private var realData: [[String : [String: String]]] = Array()
+    
     var body: some View {
+        let realData = arrayToDict(inputArray: sortedData)
         Form {
-            realData = dataConverter(inputVar: $sortedData)
             List {
-                ForEach(realData.count){ index in
-                    if sectionHeaderKeys.contains("\(realData[index].keys)") {
-                        Text(realData[index][sectionHeaderKeys.last])
-                    } else {
-                        sectionHeaderKeys.append(realData[index].keys)
-                        Section(header: Text("\(sectionHeaderKeys.last)"))
+                ForEach(realData.keys.sorted(), id: \.self) { key in
+                    Section(header: Text("\(key)")) {
+                        if let values = realData[key] {
+                            ForEach(values, id: \.self){ dict in
+                                if let text = dict["title"] {
+                                    Text(text)
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
     }
-}
-
-func dataConverter(inputVar: Binding<[[String:String]]>) -> Binding<[[String : [String: String]]]> {
-    var outArray: [[String : [String: String]]] = Array()
-    for index in inputVar.indices {
-        outArray.append( [ "\(inputVar[index]["title"]!.first!)" : inputVar[index] ] )
+    
+    func arrayToDict(inputArray: [ [String : String] ] ) ->  [String : [ [String:String] ] ] {
+        var outputDict: [String : [ [String : String] ] ] = [:]
+        for dict in inputArray {
+            let key = dict["title"]?.first
+            outputDict["\(key!)", default: [ [ : ] ] ].append(dict)
+        }
+        return outputDict
     }
-    return outArray
 }
 
 #Preview {
