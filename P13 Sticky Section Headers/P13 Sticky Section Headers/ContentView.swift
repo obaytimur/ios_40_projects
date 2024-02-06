6c69f2cb7657f6f87acb1a28aa3f32918169745d
Oğulcan Baytimur <baytimurogulcan@gmail.com>
2024-02-06 20:53:48 +0300

    feat: add scrolling side index

--- P13 Sticky Section Headers/P13 Sticky Section Headers/ContentView.swift
+++ P13 Sticky Section Headers/P13 Sticky Section Headers/ContentView.swift
@@ -12,16 +12,34 @@ struct ContentView: View {
     
     var body: some View {
         let realData = arrayToDict(inputArray: sortedData)
-        Form {
-            List {
-                ForEach(realData.keys.sorted(), id: \.self) { key in
-                    Section(header: Text("\(key)")) {
-                        if let values = realData[key] {
-                            ForEach(values, id: \.self){ dict in
-                                if let text = dict["title"] {
-                                    Text(text)
+        let letters = realData.keys.sorted()
+        ScrollViewReader { scrollProxy in
+            ZStack {
+                List {
+                    ForEach(realData.keys.sorted(), id: \.self) { key in
+                        Section(header: Text("\(key)")) {
+                            if let values = realData[key] {
+                                ForEach(values, id: \.self){ dict in
+                                    if let text = dict["title"] {
+                                        Text(text)
+                                    }
                                 }
                             }
+                        }
+                    }
+                }
+                VStack {
+                    ForEach(letters, id: \.self) { letter in
+                        HStack {
+                            Spacer()
+                            Button( action: {
+                                withAnimation {
+                                    scrollProxy.scrollTo(letter)
+                                }
+                            }, label: {
+                                Text(letter)
+                                    .font(.system(size: 15))
+                            })
                         }
                     }
                 }
